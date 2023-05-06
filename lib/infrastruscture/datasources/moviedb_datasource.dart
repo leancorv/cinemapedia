012ec7771b198e65554d109dee2d3e66badfcac7
Leandro Corvalan <leancorv@gmail.com>
2023-05-06 00:51:29 -0300

    Mapping MovieDB response

--- lib/infrastruscture/datasources/moviedb_datasource.dart
+++ lib/infrastruscture/datasources/moviedb_datasource.dart
@@ -1,7 +1,11 @@
+import 'package:dio/dio.dart';
+
 import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+
+import 'package:cinemapedia/infrastruscture/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastruscture/models/moviedb/moviedb_response.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
-import 'package:dio/dio.dart';
 
 class MoviedbDatasource extends MoviesDatasource {
   final dio = Dio(BaseOptions(
@@ -14,7 +18,13 @@ class MoviedbDatasource extends MoviesDatasource {
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     final response = await dio.get('/movie/now_playing');
-    final List<Movie> movies = [];
+    final movieDBResponse = MovieDbResponse.fromJson(response.data);
+
+    final List<Movie> movies = movieDBResponse.results
+        .where((moviedb) => moviedb.posterPath != 'no-poster')
+        .map((moviedb) => MovieMapper.movieDBToEntity(moviedb))
+        .toList();
+
     return movies;
   }
 }
