26363027509069162233aa60ce960e7f676655c7
leancorv <lecorvalan2@gmail.com>
2023-05-12 19:31:05 -0300

    SearchDelegete - Suggestions

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,11 +1,13 @@
 import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CustomAppbar extends StatelessWidget {
+class CustomAppbar extends ConsumerWidget {
   const CustomAppbar({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme.titleMedium;
     return SafeArea(
@@ -30,12 +32,15 @@ class CustomAppbar extends StatelessWidget {
                   const Spacer(),
                   IconButton(
                       onPressed: () {
+                        final movieRepository =
+                            ref.read(movieRepositoryProvider);
                         showSearch(
                           context: context,
-                          delegate: SearchMovieDelegate(),
+                          delegate: SearchMovieDelegate(
+                              searchMovies: movieRepository.searchMovies),
                         );
                       },
-                      icon: Icon(Icons.search))
+                      icon: const Icon(Icons.search))
                 ],
               )),
         ));
