5778bf4f8a3c39afa671bb4aa6c5eed8930418d9
leancorv <lecorvalan2@gmail.com>
2023-05-15 15:35:15 -0300

    Maintain a state with the searched movies

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -34,20 +34,17 @@ class CustomAppbar extends ConsumerWidget {
                   const Spacer(),
                   IconButton(
                       onPressed: () {
-                        final movieRepository =
-                            ref.read(movieRepositoryProvider);
+                        // TODO:
+                        // final searchedMovies = ref.read(searchedMoviesProvider);
                         final searchQuery = ref.read(searchQueryProvider);
 
                         showSearch<Movie?>(
                           query: searchQuery,
                           context: context,
                           delegate: SearchMovieDelegate(
-                            searchMovies: (query) {
-                              ref
-                                  .read(searchQueryProvider.notifier)
-                                  .update((state) => query);
-                              return movieRepository.searchMovies(query);
-                            },
+                            searchMovies: ref
+                                .read(searchedMoviesProvider.notifier)
+                                .searchMoviesByQuery,
                           ),
                         ).then((movie) {
                           if (movie == null) return;
