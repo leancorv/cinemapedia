acd677068e9032b8dd78b40f761a670fb3fff6aa
leancorv <lecorvalan2@gmail.com>
2023-05-15 14:55:49 -0300

    Search Query Movies Provider added

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -36,10 +36,19 @@ class CustomAppbar extends ConsumerWidget {
                       onPressed: () {
                         final movieRepository =
                             ref.read(movieRepositoryProvider);
+                        final searchQuery = ref.read(searchQueryProvider);
+
                         showSearch<Movie?>(
+                          query: searchQuery,
                           context: context,
                           delegate: SearchMovieDelegate(
-                              searchMovies: movieRepository.searchMovies),
+                            searchMovies: (query) {
+                              ref
+                                  .read(searchQueryProvider.notifier)
+                                  .update((state) => query);
+                              return movieRepository.searchMovies(query);
+                            },
+                          ),
                         ).then((movie) {
                           if (movie == null) return;
 
