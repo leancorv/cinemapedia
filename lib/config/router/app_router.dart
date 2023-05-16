9ef883a073136618773156af816ee199785eade0
leancorv <lecorvalan2@gmail.com>
2023-05-16 16:11:42 -0300

    ShellRoute - GoRouter

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -2,19 +2,51 @@ import 'package:go_router/go_router.dart';
 import 'package:cinemapedia/presentation/screens/screens.dart';
 import 'package:cinemapedia/presentation/views/views.dart';
 
-final appRouter = GoRouter(initialLocation: '/', routes: [
-  GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(childView: HomeView()),
-      routes: [
-        GoRoute(
-          path: 'movie/:id',
-          name: MovieScreen.name,
-          builder: (context, state) {
-            final movieId = state.pathParameters['id'] ?? 'no-id';
-            return MovieScreen(movieId: movieId);
-          },
-        ),
-      ]),
-]);
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    ShellRoute(
+        builder: (context, state, child) {
+          return HomeScreen(childView: child);
+        },
+        routes: [
+          GoRoute(
+            path: '/',
+            builder: (context, state) {
+              return const HomeView();
+            },
+            routes: [
+              GoRoute(
+                path: 'movie/:id',
+                name: MovieScreen.name,
+                builder: (context, state) {
+                  final movieId = state.pathParameters['id'] ?? 'no-id';
+                  return MovieScreen(movieId: movieId);
+                },
+              ),
+            ],
+          ),
+          GoRoute(
+            path: '/favorites',
+            builder: (context, state) {
+              return const FavoritesView();
+            },
+          ),
+        ]),
+    // Rutas padre/hijo
+    // GoRoute(
+    //     path: '/',
+    //     name: HomeScreen.name,
+    //     builder: (context, state) => const HomeScreen(childView: HomeView()),
+    //     routes: [
+    //       GoRoute(
+    //         path: 'movie/:id',
+    //         name: MovieScreen.name,
+    //         builder: (context, state) {
+    //           final movieId = state.pathParameters['id'] ?? 'no-id';
+    //           return MovieScreen(movieId: movieId);
+    //         },
+    //       ),
+    //     ]),
+  ],
+);
