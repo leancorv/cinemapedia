5bf19313489eb2a06415c42d3174a785ecbe08fd
leancorv <lecorvalan2@gmail.com>
2023-05-22 03:08:10 -0300

    favoriteMovies -Message to empty case added

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,6 +1,7 @@
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../providers/providers.dart';
 
@@ -39,6 +40,31 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
   @override
   Widget build(BuildContext context) {
     final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
+
+    if (favoriteMovies.isEmpty) {
+      final colors = Theme.of(context).colorScheme;
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.favorite_outline_sharp, size: 60, color: colors.primary),
+            Text(
+              'Ohh no!!',
+              style: TextStyle(fontSize: 30, color: colors.primary),
+            ),
+            const Text(
+              'No tienes películas favoritas',
+              style: TextStyle(fontSize: 20, color: Colors.black45),
+            ),
+            const SizedBox(height: 20),
+            FilledButton.tonal(
+                onPressed: () => context.go('/home/0'),
+                child: const Text('Empieza a buscar'))
+          ],
+        ),
+      );
+    }
 
     return Scaffold(
         body: MovieMasonry(
