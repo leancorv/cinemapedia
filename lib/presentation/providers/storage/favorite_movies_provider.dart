d4238925c35fb9d7382b897cc3d1b727ef8ab21d
leancorv <lecorvalan2@gmail.com>
2023-05-22 03:00:33 -0300

    toggleFavorite dinamic reaction added

--- lib/presentation/providers/storage/favorite_movies_provider.dart
+++ lib/presentation/providers/storage/favorite_movies_provider.dart
@@ -37,4 +37,16 @@ class StorageMoviesNotifier extends StateNotifier<Map<int, Movie>> {
 
     return movies;
   }
+
+  Future<void> toggleFavorite(Movie movie) async {
+    await localStorageRepository.toggleFavorite(movie);
+    final bool isMovieInFavorites = state[movie.id] != null;
+
+    if (isMovieInFavorites) {
+      state.remove(movie.id);
+      state = {...state};
+    } else {
+      state = {...state, movie.id: movie};
+    }
+  }
 }
