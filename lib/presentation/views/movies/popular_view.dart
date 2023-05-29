4cc139728b3f0ff0ffedf6cd5b13fa49c99feacb
leancorv <lecorvalan2@gmail.com>
2023-05-29 16:44:31 -0300

    popular movies added

--- lib/presentation/views/movies/popular_view.dart
+++ lib/presentation/views/movies/popular_view.dart
@@ -1,15 +1,23 @@
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/presentation/widgets/movies/movie_masonry.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PopularView extends StatelessWidget {
+class PopularView extends ConsumerWidget {
   const PopularView({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final popularMovies = ref.watch(popularMoviesProvider);
+
+    if (popularMovies.isEmpty) {
+      return const Center(child: CircularProgressIndicator(strokeWidth: 2));
+    }
     return Scaffold(
       body: MovieMasonry(
-        // loadNextPage: loadNextPage,
-        movies: [],
+        loadNextPage: () =>
+            ref.read(popularMoviesProvider.notifier).loadNextPage(),
+        movies: popularMovies,
       ),
     );
   }
