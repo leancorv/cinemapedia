26deb6f0494841b4eff84e6cf2a656a3eb1fdd15
leancorv <lecorvalan2@gmail.com>
2023-05-29 02:33:55 -0300

    random masonry animation added

--- lib/presentation/widgets/movies/movie_poster_link.dart
+++ lib/presentation/widgets/movies/movie_poster_link.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
@@ -9,13 +11,18 @@ class MoviePosterLink extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final random = Random();
     return FadeInUp(
+      from: random.nextInt(100) + 80,
+      delay: Duration(milliseconds: random.nextInt(450) + 0),
       child: GestureDetector(
         onTap: () => context.push('/home/0/movie/${movie.id}'),
         child: ClipRRect(
           borderRadius: BorderRadius.circular(20),
-          child: FadeIn(
-            child: Image.network(movie.posterPath),
+          child: FadeInImage(
+            fit: BoxFit.cover,
+            placeholder: const AssetImage('assets/loaders/mix-logo.png'),
+            image: NetworkImage(movie.posterPath),
           ),
         ),
       ),
